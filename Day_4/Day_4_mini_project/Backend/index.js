cf2c602efb4202a4bc99dc5374a095b7f6726818
Doh Landrine <landrinedoh22@gmail.com>
2025-07-25 00:43:26 +0100

    Day 4 Fix CORS middleware placement in index.js

--- Day_4/Day_4_mini_project/Backend/index.js
+++ Day_4/Day_4_mini_project/Backend/index.js
@@ -1,13 +1,15 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
-app.use(cors());
+
 const app = express();
 const port = process.env.PORT || 4000;
 const mongoose = require('mongoose');
 
 const ledsRouter = require('./routes/leds_route');
 const wifiRouter = require('./routes/wifi_route');
+
+app.use(cors());
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
